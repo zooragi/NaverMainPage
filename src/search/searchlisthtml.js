ca9fdfe1c4e134d746b79fb89bc774034aa2ca96
eldehd9898 <eldehd9898@gmail.com>
2020-12-24 08:00:54 +0900

    feat : add delete button function

--- src/search/searchlisthtml.js
+++ src/search/searchlisthtml.js
@@ -1,30 +1,23 @@
 import { month, day } from './date.js'
 
 let searchListHtml = function(searchTerm){
-    function searchValueDelete(event)  {
-        const qs = (x) => document.querySelector(x);
-        const $searchLists = qs(".kwd_list");
-        // $searchLists.childNodes.forEach(element => console.log(element));
-        console.log(event)
-    }
-    return function(){
-        return `<li class="kwd_item">
-        <div class="kwd">
-            <span class="fix">                                                        
-                <span class="kwd_ico">
-                    <i class="search_ico"></i>
-                </span>
-                <span class="ramen">${searchTerm}</span>
+    return `<li class="kwd_item">
+    <div class="kwd">
+        <span class="fix">                                                        
+            <span class="kwd_ico">
+                <i class="search_ico"></i>
             </span>
-        </div>
-        <span class="etc">
-            <span class="date">${month}.${day}</span>
-            <div class="item_del_bt" onclick=${"searchValueDelete(this)"}>
-                <span class="item_del_bt_ico imsc"></span>
-            </div>
+            <span class="ramen">${searchTerm}</span>
         </span>
+    </div>
+    <span class="etc">
+        <span class="date">${month}.${day}</span>
+        <div class="item_del_bt">
+            <span class="item_del_bt_ico imsc"></span>
+        </div>
+    </span>
     </li>`
-    }
+    
 }
 
 export {searchListHtml}
--- src/search/searchvaluedelete.js
+++ src/search/searchvaluedelete.js
@@ -3,8 +3,11 @@ import {searchValue} from "./searchwindow.js"
 let searchValueDelete = function(event)  {
     const qs = (x) => document.querySelector(x);
     const $searchLists = qs(".kwd_list");
-    // $searchLists.childNodes.forEach(element => console.log(element));
-    console.log(event)
+    const $ramen = qs(".ramen");
+    const $kwd_list = qs(".kwd_list");
+    let clickedValue = event.currentTarget.parentNode.parentNode.getElementsByClassName("ramen")[0].innerText;
+    searchValue.splice(searchValue.indexOf(clickedValue),1);
+    $kwd_list.removeChild(event.currentTarget.parentNode.parentNode);
     console.log(searchValue)
 }
 
