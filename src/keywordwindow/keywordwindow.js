fc9c0a70ebd0c8b4fde4af48534671be0069195b
eldehd9898 <eldehd9898@gmail.com>
2021-02-01 12:55:22 +0900

    feat : Show keyword when click

--- src/keywordwindow/keywordwindow.js
+++ src/keywordwindow/keywordwindow.js
@@ -1,9 +1,9 @@
 let keywordWindow = function(){
     const qs = x => document.querySelector(x);
     const realTimeViewport = qs(".real_time_viewport");
-
+    const keywordListView = qs(".rts_rank_list_view");
     realTimeViewport.addEventListener("click",(e)=>{
-        
+        keywordListView.hidden = false;
     })
 }
 
