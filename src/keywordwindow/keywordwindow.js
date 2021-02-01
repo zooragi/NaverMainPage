de9385bc73497d5d1c6530f3c1295b1cc709344c
eldehd9898 <eldehd9898@gmail.com>
2021-02-01 13:39:45 +0900

    feat : Implement the ability to open and close realtimewindow

--- src/keywordwindow/keywordwindow.js
+++ src/keywordwindow/keywordwindow.js
@@ -2,8 +2,12 @@ let keywordWindow = function(){
     const qs = x => document.querySelector(x);
     const realTimeViewport = qs(".real_time_viewport");
     const keywordListView = qs(".rts_rank_list_view");
+    const closeButton = qs(".list_view_close_btn_ico");
     realTimeViewport.addEventListener("click",(e)=>{
         keywordListView.hidden = false;
+    })
+    closeButton.addEventListener("click",(e)=>{
+        keywordListView.hidden = true;
     })
 }
 
